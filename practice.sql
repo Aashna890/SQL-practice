21a9f52f41a685b24b0c5bee22207f70ef2d6f3a
yoonaa42 <minyoonaa42@gmail.com>
2026-08-18 19:16:23 +0530

    initial commit

---
+++ practice.sql
@@ -0,0 +1,235 @@
+
+
+create table Student1(
+studentId int,
+name varchar(50),
+age int,
+dept varchar(20)
+)
+truncate table Student1
+select * from Student1
+insert into student1
+values (1,'A',20,'CMPN'),
+       (2,'B',20,'CMPN'),
+	   (3,'C',20,'CMPN'),
+	   (4,'D',21,'INFT')
+delete from student1
+where studentID=1
+alter table student1 add email varchar(40) unique
+update student1
+set email='abc@gmail.com'
+where studentID=1
+alter table student1 rename column name to Full_Name
+
+create table learners(
+studentid int primary key,
+name varchar (50),
+age int,
+email varchar (70) unique
+)
+insert into learners
+values (1,'Amit Sharma',17,'amit17@gmail.com'),
+		(2,'Anjali Verma',18,'anjali18@gmail.com'),
+		(3,'Ravi Yadav',19,'ravi19@gmail.com')
+select * from learners
+delete from learners where age<18
+truncate table 
+-----------------------------------------------------------------------------
+create table company(
+EmpName varchar(50) not Null,
+EmpId int primary key not null,
+age int,
+email varchar(40),
+gender char(1),
+check (gender in('M','F'))
+)
+insert into company 
+values ('Anjali',349,35,'anjaligaikwad@gmail.com','F'),
+		('sunita',1934,40,'sunitadahate@gmail.com','F'),
+		('sangeeta',1976,48,'sangeetaKhadge@gmail.com','F'),
+		('Akshay',971,50,'akshaykumar@gmail.com','M'),
+		('yogesh',781,51,'yogeshtiwari@yahoo.com','M')
+---
+select * from company
+---
+alter table company add job_id varchar(90)
+----
+update company 
+set job_id=case empid
+when 349 then 'senior developer'
+when 874 then 'Manager'
+when 781 then 'Manager'
+when 971 then 'Developer'
+when 1976 then 'Clerk'
+end
+---
+alter table company add salary decimal(8,2)
+update company
+set salary=case job_id
+when 'senior developer' then 75000.00
+when 'Manager' then 68000.00
+when 'Developer' then 56000.00
+when 'Clerk' then 48000.00
+end
+----
+update company set EmpID=874 where Empname = 'sunita'
+---
+insert into company 
+values ('abc',34,23,'xyz','M')
+delete from company
+where Empname='abc'
+----
+select empid,empname,job_id,salary from company
+where salary not in ( select salary from company where job_id='Manager')
+and job_id <>'Manager'
+-----
+
+--------
+create table students(
+name varchar(10) default 'UNKNOWN',
+Sid int primary key,
+departid int references departments(departid)
+)
+--------
+create table departments(
+Dname varchar(50),
+departid int primary key
+)
+--------
+insert into students
+values (default, 123),
+		('amey',54)
+-----
+select * from students
+drop table students
+drop table departments 
+
+create table depart(
+depid int primary key,
+depart_name varchar(50)
+)
+create table emp1(
+empname varchar(50),
+empid int primary key,
+depid int references depart(depid)
+)
+insert into depart
+values (1,'developer'),
+		(2,'senior developer'),
+		(3,'Manager')
+insert into emp1 values ('anjali',48,1)
+insert into emp1
+values ('ravi',34,1),
+	('anup',55,2)
+
+select * from emp1	
+
+select e.empname,d.depart_name
+from emp1 as e
+join depart as d
+on e.depid=d.depid
+---------leetcode----------------
+create table address(
+addressid int primary key,
+personid int references person(personid),
+city varchar(90),
+state varchar(50)
+)
+create table person(
+personId int primary key,
+lastName varchar(70),
+firstName varchar(60)
+)
+
+insert into person
+values (1,'wang','allen'),
+		(2,'alice','bob')
+insert into address
+values (1,2,'new york city','new york')
+
+select p.firstName,p.lastName,a.city,a.state
+from person as p
+left join address as a
+on p.personId=a.personId
+------------------------
+alter table address
+add country char(70)
+
+update address
+set country='california'
+where addressId=1;
+
+select * from address
+
+--DESC person
+alter table company 
+add address varchar(90)
+
+update company
+set address='Navi Mumbai'
+where age=35
+select * from company
+
+
+select distinct empname from company
+
+select * from company order by age desc
+
+create table Faculty(
+Faculty_code int primary key,
+Faculty_Name varchar(70),
+dob date,
+subject varchar(10),
+hours int
+)
+
+insert into Faculty
+values (100,'Yogesh','1964/07/17','DSA',16),
+		(101,'Amit','1972/12/24','MIS',16),
+		(102,'omprakash','1980/02/03','PWRC',8),
+		(103,'Nitin','1966/11/28','DT',10)
+
+select * from faculty order by Faculty_Name
+select * from faculty order by Faculty_Name DESC
+select * from faculty order by dob desc
+select * from faculty where hours>12
+select * from faculty where hours<>16
+select * from faculty where subject='DT'
+select * from faculty where subject<>'DT' And hours<>10
+select * from faculty where subject<>'DT' or hours>10
+select * from faculty where subject<>'DT'
+select * from faculty where hours between 10 and 20
+select * from faculty where hours not between 10 and 20
+
+------------pattern matching operation--------
+select * from faculty where faculty_name like 'A%'
+select * from faculty where faculty_name like 'om%'
+select * from faculty where faculty_name like '%h'
+select * from faculty where faculty_name like '%a%'
+select * from faculty where faculty_name like '_o%e%h'
+select * from faculty where faculty_name like '_m%'
+select * from faculty where faculty_name like '%_s%'
+
+---------------restricting row (limit)------------
+select * from faculty order by hours desc limit 2
+
+create table exam(
+sid int primary key,
+sname varchar(90),
+marks int
+)
+insert into exam
+values (1,'Mahesh',90),
+		(2,'Suhas',80),
+		(3,'jyendra',89),
+		(4,'Sachin',99),
+		(5,'Vishal',88),
+		(6,'Payal',90)
+select * from exam
+select count(sid) as sid_count from exam
+select count(distinct sid) as dist_count from exam
+select count(distinct marks) as dist_marks from exam
+select avg(marks) as Avg_marks from exam
+select min(marks) as mini_marks from exam
+select max(marks) as max_marks from exam
+select sname, marks from exam where marks>89
